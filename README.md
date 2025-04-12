70073ad53d946e6bd9be03f6093cac8f0503269d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-13 02:32:23 +0530

    Implement functionality to toggle desktop icons

--- README.md
+++ README.md
@@ -13,6 +13,12 @@ g++ main.cpp -o toggle-desktop-icons.exe  -municode -mwindows
 `-municode`: to support wide-strings (unicode)
 `-mwindows`: because we use `wWinMain()` to setup a GUI application instead of a console application
 
+## References
+
+- https://stackoverflow.com/questions/6402834/how-to-hide-desktop-icons-programmatically
+- https://techcommunity.microsoft.com/discussions/windows10space/showhide-desktop-icons-with-a-single-click/2016194
+- https://www.autohotkey.com/boards/viewtopic.php?t=75890
+
 ## License
 
 This project is licensed under the [MIT License](./LICENSE)
--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include <windows.h> // Windows API
 
-// The main entrypoint of the application
-//
-// We used `wWinMain` instead of `main` to create a GUI application instead of a console application
-//
-// `hInstance`: A handle (unique ID) to the current application instance
-// `hPrevInstance`: Obsolete! Always `NULL` in modern Windows
-// `lpCmdLine`: The command-line arguments as wide-string (unicode)
-// `nCmdShow`: How the window should be shown (minimized, maximized, normal etc). Used when calling `ShowWindow()`
+/// Command ID used by Explorer to toggle desktop icons
+// https://stackoverflow.com/questions/6402834/how-to-hide-desktop-icons-programmatically
+const int TOGGLE_DESKTOP_ICONS = 0x7402;
+
+/// Finds the handle to the `SHELLDLL_DefView` responsible for the desktop icons.
+/// It may be a child of either "Progman" or a "WorkerW" window, depending on the system.
+HWND GetShellViewWindow()
+{
+    // Get the handle to the Program Manager (desktop background window)
+    HWND progman = FindWindow(L"Progman", nullptr);
+
+    // Try to find the `SHELLDLL_DefView` as a child of Progman
+    HWND defView = FindWindowEx(progman, nullptr, L"SHELLDLL_DefView", nullptr);
+
+    // If not found, try looking under WorkerW windows (some Windows versions use these)
+    if (!defView)
+    {
+        HWND workerW = nullptr;
+        do
+        {
+            // Enumerate all WorkerW windows
+            workerW = FindWindowEx(nullptr, workerW, L"workerW", nullptr);
+            // Try to find `SHELLDLL_DefView` under this WorkerW
+            defView = FindWindowEx(workerW, nullptr, L"SHELLDLL_DefView", nullptr);
+        } while (workerW && !defView);
+    }
+
+    // Return the handle to `SHELLDLL_DefView`, or nullptr if not found
+    return defView;
+}
+
+/// The main entrypoint of the application
+///
+/// We used `wWinMain` instead of `main` to create a GUI application instead of a console application
+///
+/// `hInstance`: A handle (unique ID) to the current application instance
+/// `hPrevInstance`: Obsolete! Always `NULL` in modern Windows
+/// `lpCmdLine`: The command-line arguments as wide-string (unicode)
+/// `nCmdShow`: How the window should be shown (minimized, maximized, normal etc). Used when calling `ShowWindow()`
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(NULL, L"Hello from GUI!", L"MyApp", MB_OK);
+    HWND defView = GetShellViewWindow();
+    if (defView)
+    {
+        SendMessage(defView, WM_COMMAND, (WPARAM)TOGGLE_DESKTOP_ICONS, 0);
+    }
     return 0;
 }
